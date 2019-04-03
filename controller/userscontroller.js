74c674bc6cc0a0a4f2d412786cda2c6578b8ccf0
Mohanapriya <monariya1608@gmail.com>
2019-04-03 10:08:05 +0530

    users management

--- controller/userscontroller.js
+++ controller/userscontroller.js
@@ -2,16 +2,22 @@ const router = require('express').Router();
 const usersService = require('../services/usersService')
 
 
-router.get('/:id',(req,res) =>{
-    console.log('Users')
-    res.json({message: "Im in users"})
+router.get('/:id',async(req,res) =>{
+
+    const users_table = await usersService.getUsersById(parseInt(id))
+
+    // console.log('Users')
+    res.json({users_table: users_table})
 })
 
 
-router.get('/',(req,res) =>{
-    const usersList = usersService.getUsers()
-    res.json({users: usersList})
+router.post('/', async (req, res) => {
+    try {
+        await usersService.insert(req.body)
+        res.json({ message: 'In users post'})
+    } catch (err) {
+        res.status(422).json({ message: err })
+    }
 })
-
 
 module.exports = router
--- services/usersService.js
+++ services/usersService.js
@@ -1,20 +1,46 @@
-exports.getUsers = () =>{
-    return [
-        {
-            firstName: "Vivo Y53",
-            lastName: "Vivo",
-            email: "China",
-            phone: "10,000",
-            designation: "Mona",
-            status:"Active"
-          },
-          {
-            firstName: "Vivo Y53",
-            lastName: "Vivo",
-            email: "China",
-            phone: "10,000",
-            designation: "Mona",
-            status:"Active"
-          }
-    ]
+const client = require('../db')
+
+exports.getUsers_table = async() =>{
+    const result = await client.query('select * from users_table')
+    return result.rows;
 }
+
+exports.getUsers_tableById = async(id) => {
+    const result = await client.query(`select * from users_table where id = 4`)
+    return result.rows[0]
+}
+
+exports.insert = async (users) => {
+    try {
+        const query = 'insert into users_table(username,firstname,lastname,email,mobile,designation,status,isdeleted) VALUES($1,$2,$3,$4,$5,$6,$7,$8)'
+        const values = [users.username, users.firstname,users.lastname, users.email, users.mobile, users.designation, users.status,users.isdeleted]
+        await client.query(query, values)
+    } catch (err) {
+        console.log(err)
+        throw 'Unable to insert users'
+    }
+}
+
+
+
+
+// exports.getUsers = () =>{
+//     return [
+//         {
+//             firstName: "Vivo Y53",
+//             lastName: "Vivo",
+//             email: "China",
+//             phone: "10,000",
+//             designation: "Mona",
+//             status:"Active"
+//           },
+//           {
+//             firstName: "Vivo Y53",
+//             lastName: "Vivo",
+//             email: "China",
+//             phone: "10,000",
+//             designation: "Mona",
+//             status:"Active"
+//           }
+//     ]
+// }
