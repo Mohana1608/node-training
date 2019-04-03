3a5d6da73ff775ad15fda2db256d982c99e6f931
Mohanapriya <monariya1608@gmail.com>
2019-04-03 10:07:45 +0530

    products management

--- controller/productscontroller.js
+++ controller/productscontroller.js
@@ -2,22 +2,29 @@ const router = require('express').Router();
 const productservice = require('../services/productservices')
 
 
-router.get('/',(req,res) =>{
-    const products = productservice.getProducts()
+router.get('/', async (req, res) => {
+    const products = await productservice.getProducts()
     // console.log('Products')
-    res.json({message: "Im in products"})
+    res.json({ products: products })
 })
 
-router.post('/',(req,res) =>{
+router.post('/:username/:manufacturerId', async (req, res) => {
+    try {
+        const { username, manufacturerId } = req.params
+        console.log("I'm in products")
+        await productservice.insert(username, parseInt(manufacturerId), req.body)
+        res.json({ message: "In products post" })
+    } catch (err) {
+        res.status(422).json({ message: err })
+    }
     // console.log('Products ${req.params.id}')
-
-    console.log(JSON.stringify(req.body,undefined,4));
-    res.json({message: "In products"})
+    // console.log(JSON.stringify(req.body, undefined, 4));
+    // res.json({ message: "In products" })
 })
-router.get('/:id',(req,res)=>{
-    const id = req.params.id
-    console.log(`Products ${id}`);
-    res.json({message:" In products single"})
+router.get('/:id', async (req, res) => {
+    const { id } = req.params
+    const product = await productservice.getProductById(parseInt(id))
+    res.json({ product: product })
 }
 )
 
--- services/productservices.js
+++ services/productservices.js
@@ -1,18 +1,42 @@
-exports.getProducts = () =>{
-    return[
-        {
-            name:"Oppof9pro",
-            company: "Oppo",
-            manufacturer: "Japan",
-            price:"22,000",
-            owner:"Mani"
-        },
-    {
-        name:"Oppof9pro",
-        company: "Oppo",
-        manufacturer: "Japan",
-        price:"22,000",
-        owner:"Mani" 
-    }
-]
+
+const client = require('../db')
+exports.getProducts = async () => {
+    const result = await client.query('select * from products')
+    return result.rows;
 }
+
+exports.getProductById = async (id) => {
+    const result = await client.query(`select * from products where id = ${id}`)
+    return result.rows[0]
+}
+
+exports.insert = async (userId, manufacturerId, product) => {
+    try {
+        const query = 'insert into products(name,code,m_id,price,description,quantity,created_at,created_by) VALUES($1,$2,$3,$4,$5,$6,$7, $8)'
+        const values = [product.name, product.code,manufacturerId, product.price, product.description, product.quantity, new Date(), userId]
+        await client.query(query, values)
+    } catch (err) {
+        console.log(err)
+        throw 'Unable to insert product'
+    }
+}
+
+
+// exports.getProducts = () =>{
+//     return[
+//         {
+//             name:"Oppof9pro",
+//             company: "Oppo",
+//             manufacturer: "Japan",
+//             price:"22,000",
+//             owner:"Mani"
+//         },
+//     {
+//         name:"Oppof9pro",
+//         company: "Oppo",
+//         manufacturer: "Japan",
+//         price:"22,000",
+//         owner:"Mani" 
+//     }
+// ]
+// }
